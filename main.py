9dfca713cf7052c0b027901c5a31c7414c0f5801
Varun Bale <varun.bale@gmail.com>
2021-02-19 16:51:53 +0530

    JWT

--- main.py
+++ main.py
@@ -1,23 +1,65 @@
 import os
 from flask import Flask, request, make_response, jsonify
 from firebase_admin import credentials, firestore, initialize_app
+import jwt 
+import datetime
+from functools import wraps
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') 
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 403
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 403
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({'message' : 'Anyone can view this!'})
+
+@app.route('/protected')
+@token_required
+def protected():
+    return jsonify({'message' : 'This is only available for people with valid tokens.'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'varun':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=2)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+
 
 cred = credentials.Certificate('G:\key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 
-@app.route('/')
-def index():
-    if request.authorization and request.authorization.username == 'varun'and request.authorization.password=='1234':
-        return "you are in"
-       
-    
-    return make_response('Couldnt verify you')    
+
+
+
 
 @app.route('/post',methods=["POST","GET"])
+@token_required
 def post():
     data={"name":"varun","rollno":0,"stream":"CSE"}
     db.collection("students").add(data)
@@ -31,6 +73,7 @@ def create(name,num,stream):
     return "inserted record"   
 
 @app.route("/list",methods=["GET"])
+@token_required
 def list():
     docs = db.collection('students').get()
     result=[]
